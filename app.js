0ba34848b4c2522e21202e1f97b45633f88a658e
Alex Lopez <alexlopezyauri@gmail.com>
2024-08-27 22:11:22 -0500

    actualiza js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function encriptar(){
     let img = document.getElementById("img");
     let teEcriptado = document.getElementById("salida__texto_encriptado");
     let parafo = document.getElementById("parrafo");
+    let btnEncripta = document.getElementById("output__button");
     let textoEncriptado = texto
 
         .replace(/e/gi, "enter")
@@ -22,9 +23,10 @@ function encriptar(){
         img.style.display = "none";
         teEcriptado.style.top = "5%";
         parafo.style.display = "none";
+        btnEncripta.style.position ="relative";
     }
     else{
-        alert("Ingreso tecto a encriptar");
+        alert("Ingreso el texto a encriptar/desencriptar");
     }
     
 }
@@ -65,3 +67,43 @@ function copiar(){
     let textoCopiado = document.getElementById("titulo__mensaje");
     navigator.clipboard.writeText(textoCopiado.textContent);
 }
+
+// Función para verificar si un carácter es especial
+function esCaracterPermitido(char) {
+    // Definir una expresión regular para caracteres permitidos (letras, números, y espacios)
+    const regex = /^[a-zA-Z0-9\s]$/;
+    // Retornar verdadero si el carácter es permitido, falso si es un carácter especial
+    return regex.test(char);
+}
+
+// Función que se ejecuta cuando se presiona una tecla en el textarea
+function verificarCaracter(event) {
+    // Obtener el código de la tecla presionada
+    const charCode = event.which || event.keyCode;
+    
+    // Convertir el código de la tecla a un carácter
+    const char = String.fromCharCode(charCode);
+
+    // Si el carácter no está permitido, prevenir su ingreso
+    if (!esCaracterPermitido(char)) {
+        event.preventDefault();
+        alert("no se permiten caracteres especiales");
+    }
+}
+
+document.getElementById('input').addEventListener('keypress', verificarCaracter);
+
+document.getElementById('input').addEventListener('keypress', function(event) {
+    // Obtener el código de la tecla presionada
+    const charCode = event.which || event.keyCode;
+    
+    // Convertir el código de la tecla a un carácter
+    const char = String.fromCharCode(charCode);
+    
+    // Verificar si el carácter es una letra mayúscula
+    if (char === char.toUpperCase() && char !== char.toLowerCase()) {
+        // Evitar la entrada de letras mayúsculas
+        event.preventDefault();
+        alert("no se permiten letras mayusculas");
+    }
+});
